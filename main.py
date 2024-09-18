b76b4804cca421fa3c35182808f497f735c0648a
My event horizon <88778695+MOTOR92@users.noreply.github.com>
2024-09-19 00:59:52 +0500

    Update main.py
    Added image uploading

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+import asyncio
+import os
+from data import config
+from utils.core.file_manager import get_images_from_folder
 from utils.core.telegram import Accounts
 from utils.starter import start, stats
-import asyncio
-from data import config
-import os
-
 
 async def main():
     print("Soft's author: https://t.me/ApeCryptor\n")
@@ -13,15 +13,16 @@ async def main():
         print(config.SOFT_INFO)
         return
 
-    if not os.path.exists('sessions'): os.mkdir('sessions')
+    if not os.path.exists(config.WORKDIR):
+        os.mkdir(config.WORKDIR)
 
     if config.PROXY['USE_PROXY_FROM_FILE']:
         if not os.path.exists(config.PROXY['PROXY_PATH']):
             with open(config.PROXY['PROXY_PATH'], 'w') as f:
                 f.write("")
     else:
-        if not os.path.exists('sessions/accounts.json'):
-            with open("sessions/accounts.json", 'w') as f:
+        if not os.path.exists(os.path.join(config.WORKDIR, 'accounts.json')):
+            with open(os.path.join(config.WORKDIR, 'accounts.json'), 'w') as f:
                 f.write("[]")
 
     if action == 3:
@@ -31,16 +32,17 @@ async def main():
         await stats()
 
     if action == 1:
-        accounts = await Accounts().get_accounts()
+        folder_path = config.IMAGE_FOLDER_PATH
+        images = get_images_from_folder(folder_path)
 
+        accounts = await Accounts().get_accounts()
         tasks = []
 
         for thread, account in enumerate(accounts):
             session_name, phone_number, proxy = account.values()
-            tasks.append(asyncio.create_task(start(session_name=session_name, phone_number=phone_number, thread=thread, proxy=proxy)))
+            tasks.append(asyncio.create_task(start(session_name=session_name, phone_number=phone_number, thread=thread, proxy=proxy, images=images)))
 
         await asyncio.gather(*tasks)
 
-
 if __name__ == '__main__':
-    asyncio.get_event_loop().run_until_complete(main())
+    asyncio.run(main())
